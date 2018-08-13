dfa7f075ae58e91cde7de627a8773d954e853865
Bemilton Suero <bemsuero@yahoo.com>
2018-08-13 17:00:18 -0400

    Done, doing visual tweaking

--- server.rb
+++ server.rb
@@ -1,26 +1,31 @@
 # using SendGrid;s Ruby Library
 # https://github.com/sendgrid/sendgrid-ruby
-require "sendgrid-ruby"
+# require "sendgrid-ruby"
 require "sinatra"
-include SendGrid
+require "curb"
+# include SendGrid
 
-
-def email
-from = Email.new(email: 'test@example.com')
-to = Email.new(email: 'test@example.com')
-subject = 'Sending with SendGrid is Fun'
-content = Content.new(type: 'text/plain', value: 'and easy to do anywhere, even with Ruby')
-mail = Mail.new(from, subject, to, content)
-
-sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
-response = sg.client.mail._('send').post(request_body: mail.to_json)
-puts response.status_code
-puts response.body
-puts response.headers
-end
+# def email
+# from = Email.new(email: 'test@example.com')
+# to = Email.new(email: 'test@example.com')
+# subject = 'Sending with SendGrid is Fun'
+# content = Content.new(type: 'text/plain', value: 'and easy to do anywhere, even with Ruby')
+# mail = Mail.new(from, subject, to, content)
+#
+# sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+# response = sg.client.mail._('send').post(request_body: mail.to_json)
+# puts response.status_code
+# puts response.body
+# puts response.headers
+# end
 
 get '/' do
   erb :home
+end
+
+get '/search_results' do
+  @search = params[:search_bar]
+  erb :search_results
 end
 
 get '/cookies' do
@@ -42,18 +47,30 @@ get '/signup' do
   erb :signup
 end
 
+get '/events' do
+  @res = params[:query]
+  # @web_search = Curl::Easy.perform("https://www.eventbriteapi.com/v3/events/search/?token=F33FG3NCO5EPWSKXWJXF") #with images
+  @web_search = Curl::Easy.perform("https://www.eventbriteapi.com/v3/events/search/?q=baking&location.address=new+york&token=F33FG3NCO5EPWSKXWJXF") # with search function
+  @look_through = @web_search.body_str
+  @data = JSON.parse(@look_through)
+  @events = @data["events"]
+  erb :events
+end
 
 get '/thecall' do
   erb :thecall
 end
 
-class Food
-  @@food_count = 0
+$cookies = []
+$muffins = []
+$cakes = []
+
+class BakeryTreat
+  attr_accessor :name, :cost
   def initialize(name, cost)
     @name = name
     @cost = cost
     @description = "Food for Sale"
-    @@food_count += 1
 end
 
   def self.count
@@ -61,26 +78,47 @@ end
 end
 end
 
-class Cookies < Food
-  def initialize(name, cost, calories)
+class Cookie < BakeryTreat
+  attr_accessor :name, :cost, :calories, :description
+  def initialize(name, cost, calories, description)
     super(name, cost)
-    @description = "Yes"
+    @description = description
     @calories = calories
+    $cookies << self
 end
 end
 
-class Cakes < Food
-  def initialize(name, cost, calories)
+class Cake < BakeryTreat
+    attr_accessor :name, :cost, :calories, :description
+  def initialize(name, cost, calories, description)
     super(name, cost)
-    @description = "No"
+    @description = description
     @calories = calories
+    $cakes << self
   end
 end
 
-class Muffins < Food
-  def initialize(name, cost, calories)
+class Muffin < BakeryTreat
+    attr_accessor :name, :cost, :calories, :description
+  def initialize(name, cost, calories, description)
     super(name, cost)
-    @description = "No"
+    @description = description
     @calories = calories
+    $muffins << self
   end
 end
+
+@chocolate_chip = Cookie.new("Chocolate Chip Cookie", 2, 300, "A rich and creamy chocolate chip cookie. Dipped in the finest chocolate sauce.")
+@peanut_butter = Cookie.new("Peanut Butter Cookie", 2, 500, "Cookies baked with peanut butter then smothered in a peanut butter sauce.")
+@butter_chip = Cookie.new("Butter Cookie", 2, 100, "Grandma's Favorite! A simple classic.")
+@ginger_bread = Cookie.new("Gingerbread Cookie", 2, 200, "Flavored with ginger, cloves, cinnamon and sweetened with honey, these cookies are out of this world!")
+
+@chocolate_chip_cake = Cake.new("Chocolate Chip Cake", 2, 300, "A rich and creamy chocolate chip cookie. Dipped in the finest chocolate sauce.")
+@peanut_butter_cake = Cake.new("Peanut Butter Cake", 2, 500, "Cakes baked with peanut butter then smothered in a peanut butter sauce.")
+@butter_chip_cake = Cake.new("Butter Cake", 2, 100, "Grandma's Favorite! A simple classic.")
+@ginger_bread_cake = Cake.new("Gingerbread Cake", 2, 200, "Flavored with ginger, cloves, cinnamon and sweetened with honey, these cookies are out of this world!")
+
+@chocolate_chip_muffin = Muffin.new("Chocolate Chip Muffin", 2, 300, "A rich and creamy chocolate chip cookie. Dipped in the finest chocolate sauce.")
+@peanut_butter_muffin = Muffin.new("Peanut Butter Muffin", 2, 500, "Muffins baked with peanut butter then smothered in a peanut butter sauce.")
+@butter_chip_muffin = Muffin.new("Butter Muffin", 2, 100, "Grandma's Favorite! A simple classic.")
+@ginger_bread_muffin = Muffin.new("Gingerbread Muffin", 2, 200, "Flavored with ginger, cloves, cinnamon and sweetened with honey, these cookies are out of this world!")
