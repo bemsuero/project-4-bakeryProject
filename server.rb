5ad8073c94d484426e17437ec20c00b38a924241
Bemilton Suero <bemsuero@yahoo.com>
2018-08-15 10:07:00 -0400

    adjusted navBar, changed some visuals

--- server.rb
+++ server.rb
@@ -114,15 +114,15 @@ end
 
 @chocolate_chip = Cookie.new("Chocolate Chip Cookie", 2, 300, "/images/chocolate.jpg", "A rich and creamy chocolate chip cookie. Dipped in the finest chocolate sauce.")
 @peanut_butter = Cookie.new("Peanut Butter Cookie", 2, 500, "/images/peanut.jpg", "Cookies baked with peanut butter then smothered in a peanut butter sauce.")
-@butter_chip = Cookie.new("Butter Cookie", 2, 100, "/images/butter.jpg", "Grandma's Favorite! A simple classic.")
+@butter_cookie = Cookie.new("Sugar Cookie", 2, 100, "/images/butter.jpg", "Grandma's Favorite! A simple classic.")
 @ginger_bread = Cookie.new("Gingerbread Cookie", 2, 200, "/images/ginger.jpg", "Flavored with ginger, cloves, cinnamon and sweetened with honey, these cookies are out of this world!")
 
-@chocolate_chip_cake = Cake.new("Chocolate Chip Cake", 2, 300, "/images/chocolatecake.jpg", "A rich and creamy chocolate chip cake. Dipped in the finest chocolate sauce.")
-@peanut_butter_cake = Cake.new("Peanut Butter Cake", 2, 500, "/images/peanutcake.jpg", "Cakes baked with peanut butter then smothered in a peanut butter sauce.")
-@butter_chip_cake = Cake.new("Butter Cake", 2, 100, "/images/buttercake.jpg", "Grandma's Favorite! A simple classic.")
-@ginger_bread_cake = Cake.new("Gingerbread Cake", 2, 200, "/images/gingercake.jpg", "Flavored with ginger, cloves, cinnamon and sweetened with honey, these cakes are out of this world!")
+@chocolate_cake = Cake.new("Chocolate Chip Cake", 2, 300, "/images/chocolatecake.jpg", "Moist, delicious chocolate cake for any celebration.")
+@peanut_butter_cake = Cake.new("Peanut Butter Cake", 2, 500, "/images/peanutcake.jpg", "Very moist and fluffy! The flavors are the perfect blend of chocolate, bananas, and Nutella. Scrumptious!")
+@lemon_buttermilk_cake = Cake.new("Lemon Buttermilk Pound Cake", 2, 100, "/images/buttercake.jpg", "This is not your daughter's cake. No triple-threat chocolate, caramel, salted, nouveau let-it-sit-for-a-day-so-the-flavor a-can-blend cake here. This is simply a fabulous old-school cake, with the perfect moistness and density of a pound-cake-gone-right.")
+@ginger_bread_cake = Cake.new("Gingerbread Cake", 2, 200, "/images/gingercake.jpg", "This is a wonderful, moist easy cake with perfect texture. It's so nice to have a festive winter dessert that doesn't take butter. The glaze over the warm cake is the perfect touch.")
 
-@chocolate_chip_muffin = Muffin.new("Chocolate Chip Muffin", 2, 300, "/images/chocolatemuffin.jpg", "A rich and creamy chocolate chip muffin. Dipped in the finest chocolate sauce.")
-@peanut_butter_muffin = Muffin.new("Peanut Butter Muffin", 2, 500, "/images/peanutmuffin.jpg", "Muffins baked with peanut butter then smothered in a peanut butter sauce.")
-@butter_chip_muffin = Muffin.new("Butter Muffin", 2, 100, "/images/buttermuffin.jpg", "Grandma's Favorite! A simple classic.")
+@chocolate_chip_muffin = Muffin.new("Chocolate Chip Muffin", 2, 300, "/images/chocolatemuffin.jpg", "These muffins are a chocolatey treat thanks to the double chocolate punch of cocoa powder and chocolate chips.")
+@peanut_butter_muffin = Muffin.new("Peanut Butter Muffin", 2, 500, "/images/peanutmuffin.jpg", "Oat muffins with a hint of peanut butter, make a great on-the-go breakfast or snack.")
+@mini_pumpkin_butterscotch_muffins = Muffin.new("Mini Pumpkin Butterscotch Muffin", 2, 100, "/images/buttermuffin.jpg", "These moist and deliciously-sweet pumpkin muffins are great for sharing, but you won't want to share! Don't let the pumpkin-butterscotch combination fool you, these taste great!")
 @ginger_bread_muffin = Muffin.new("Gingerbread Muffin", 2, 200, "/images/gingermuffin.jpg", "Flavored with ginger, cloves, cinnamon and sweetened with honey, these muffins are out of this world!")
